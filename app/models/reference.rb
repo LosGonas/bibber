e1614973e996543c415294449f788e006313f2c7
Elias Nygren <elias.nygren@outlook.com>
2014-04-01 20:14:21 +0300

    references in user show

--- app/models/reference.rb
+++ app/models/reference.rb
@@ -4,4 +4,9 @@ class Reference
   key :entry_type, String
   key :entries, Hash
 
+
+
+  def to_bib(id="KB04")
+    "@#{entry_type} {#{id},\n#{entries.map {|x|"  #{x[0]} = \"#{x[1]}\""}.join(",\n")}\n}"
+  end
 end
